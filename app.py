6b51b09e27b6bfc394d55e35ae60470d766a3257
shreyash4803 <shreyash4803@gmail.com>
2024-12-16 00:23:00 +0530

    vercel

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ from routes.routes import create_todo_routes
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
 client = MongoClient(app.config["MONGO_URI"])
 db = client.todo_db
 
-
 app.register_blueprint(create_todo_routes(db))
 
-if __name__ == "__main__":
-    app.run()
+# Vercel requires Flask apps to return WSGI-compatible app in function
+def handler(request):
+    return app(request)
